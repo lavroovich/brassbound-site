b84a248e83935d96398f4ea07ef4c58c26c4b815
lavroovich <168745434+lavroovich@users.noreply.github.com>
2025-10-17 20:42:03 +0700

    Prepare project for Vercel

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def main():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "5005"))
+    debug = os.getenv("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
